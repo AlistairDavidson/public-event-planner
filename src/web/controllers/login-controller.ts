b254caa60cca173b0083fcc6a7b214a0d9e9a685
Alistair Davidson <alistair.davidson@snp.org>
2017-07-22 20:39:28 +0100

    Trying to get itget it working

--- src/web/controllers/login-controller.ts
+++ src/web/controllers/login-controller.ts
@@ -17,10 +17,27 @@ export default class LoginController {
         res.render('login.ejs', { message: req.flash('loginMessage') }); 
     }
 
+    @POST('/login')
+    async loginPost(req: express.Request, res: express.Response) {
+        return passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        });
+    }
+
     @GET('/signup')    
     async signup(req: express.Request, res: express.Response) {
         res.render('signup.ejs', { message: req.flash('signupMessage') }); 
     }
+
+    @POST('/signup')
+    async signupPost(req: express.Request, res: express.Response) {
+        console.log('signup', req.body);
+        return passport.authenticate('local-signup')(req, res, function () {
+            res.redirect('/profile');
+        }); 
+    } 
 
     @GET('/logout')    
     async logout(req: express.Request, res: express.Response) {
@@ -32,12 +49,4 @@ export default class LoginController {
     async profile(req: express.Request, res: express.Response) {
         res.render('profile.ejs', { user: req.user.get() }); 
     }
-    
-    @POST('/signup')
-    async signupPost(req: express.Request, res: express.Response) {
-        console.log('signup', req.body);
-        passport.authenticate('local-signup')(req, res, function () {
-            res.redirect('/profile');
-        }); 
-    } 
 }
