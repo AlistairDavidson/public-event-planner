0a1ea12d24c7c1ab0c69543045c30425e0c3e932
Alistair Davidson <alistair.davidson@snp.org>
2017-07-22 16:48:26 +0100

    Remove unused variable

--- src/common/database.ts
+++ src/common/database.ts
@@ -10,7 +10,7 @@ export class Database {
         this.models = models;
     }
 
-    connect(no_salesforce = false) {
+    connect() {
         this.db = new SequelizeStatic(
             process.env.DATABASE_URL, {
                 native: true
@@ -24,7 +24,7 @@ export class Database {
                 }, function (err) { 
                     console.log('Unable to connect to the database:', err);
                 })
-                .then(() => models.init(no_salesforce))            
+                .then(() => models.init())            
                 .then(() => this.db.sync());
         } catch(e) {
             console.error(e);
