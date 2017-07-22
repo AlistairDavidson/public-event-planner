1acd108d1a9ed98cad481660dcd70514e264026b
Alistair Davidson <alistair.davidson@snp.org>
2017-07-22 21:53:01 +0100

    Trying to get itget it working

--- src/web/controllers/login-controller.ts
+++ src/web/controllers/login-controller.ts
@@ -3,7 +3,18 @@ import * as passport from 'passport';
 import { Auth, GET, POST } from '../services/web-decorators';
 
 export default class LoginController {
-    constructor() {
+    constructor(app: express.Application) {
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }));
+
+        app.post('/signup', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }));
     }
 
     @GET('/')    
@@ -17,27 +28,10 @@ export default class LoginController {
         res.render('login.ejs', { message: req.flash('loginMessage') }); 
     }
 
-    @POST('/login')
-    async loginPost(req: express.Request, res: express.Response) {
-        return passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash: true // allow flash messages
-        });
-    }
-
     @GET('/signup')    
     async signup(req: express.Request, res: express.Response) {
         res.render('signup.ejs', { message: req.flash('signupMessage') }); 
     }
-
-    @POST('/signup')
-    async signupPost(req: express.Request, res: express.Response) {
-        console.log('signup', req.body);
-        return passport.authenticate('local-signup')(req, res, function () {
-            res.redirect('/profile');
-        }); 
-    } 
 
     @GET('/logout')    
     async logout(req: express.Request, res: express.Response) {
