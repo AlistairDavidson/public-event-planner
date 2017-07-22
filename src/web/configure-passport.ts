463f163be4248c2cec04c5d163e965c74c52804c
Alistair Davidson <alistair.davidson@snp.org>
2017-07-22 21:21:20 +0100

    Login

--- src/web/configure-passport.ts
+++ src/web/configure-passport.ts
@@ -52,3 +52,35 @@ export default function(passport: passport.Passport) {
         })
     }
 }
+
+
+passport.use('local-login', new LocalStrategy({
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+},
+    function(req: express.Request, username: string, password: string, done: Function) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        database.models.User.findOne({
+            where: {
+                username: username
+            }
+        }).then((user) => {
+            // if no user is found, return the message
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            }
+            
+            // if the user is found but the password is wrong
+            if (!authService.validPassword(user, password)) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+            }
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+
+    }));
+
+};
+
