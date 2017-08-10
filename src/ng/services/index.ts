96c6e8e77e6d2cbcb78c4687602e5c861f106bd2
Alistair Davidson <alistair.davidson@snp.org>
2017-08-10 22:11:35 +0100

    Tables

--- src/ng/services/index.ts
+++ src/ng/services/index.ts
@@ -1,7 +1,9 @@
 import { module } from 'angular';
 import applicationService from './application-service';
 import eventService from './event-service';
+import actService from './act-service';
 
 export default module('event-planner.services', [])
     .service('applicationService', applicationService)
+    .service('actService', actService)
     .service('eventService', eventService);
